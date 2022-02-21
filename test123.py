6752f8f3b1d806a49680a8415b9d69cb9c4800ec
Bishal Karki <bishal.bishal2050@gmail.com>
2022-02-21 19:08:08 +0545

    changes made

--- test123.py
+++ test123.py
@@ -26,20 +26,20 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(result,value,"Does not match")
 
 
-    # def testcontact(self):
-    #     contact = self.driver.find_element_by_xpath("//*[@class='navbar-collapse']/ul/li[2]/a")
-    #     contact.click()
-    #     time.sleep(5)
-    #     self.driver.find_element_by_id("recipient-email").send_keys("Test1")
-    #     self.driver.find_element_by_id("recipient-name").send_keys("Test Name")
-    #     self.driver.find_element_by_id("message-text").send_keys("Message 1")
-    #     self.driver.find_element_by_xpath("//*[@id='exampleModal']/div/div/div[3]/button[2]").click()
-    #     time.sleep(5)
-    #     self.driver.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/button/span").click()
-    #     # driver.find_element_by_id("recipient-email").send_keys("Test1")
-    #     # driver.find_element(By.ID,"recipient-email").send_keys("BY New one")
-    #     time.sleep(5)
-    #     self.driver.switch_to.alert.accept()
+    def testcontact(self):
+        contact = self.driver.find_element_by_xpath("//*[@class='navbar-collapse']/ul/li[2]/a")
+        contact.click()
+        time.sleep(5)
+        self.driver.find_element_by_id("recipient-email").send_keys("Test1")
+        self.driver.find_element_by_id("recipient-name").send_keys("Test Name")
+        self.driver.find_element_by_id("message-text").send_keys("Message 1")
+        self.driver.find_element_by_xpath("//*[@id='exampleModal']/div/div/div[3]/button[2]").click()
+        time.sleep(5)
+        self.driver.find_element_by_xpath("/html/body/div[1]/div/div/div[1]/button/span").click()
+        # driver.find_element_by_id("recipient-email").send_keys("Test1")
+        # driver.find_element(By.ID,"recipient-email").send_keys("BY New one")
+        time.sleep(5)
+        self.driver.switch_to.alert.accept()
 
 
 
